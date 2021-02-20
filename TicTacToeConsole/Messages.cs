5e5a68acd74ea00f6ce9bb5847e9b15a7e938a1f
Martin Todorov <todorovmartin@outlook.com>
2021-02-20 22:20:16 +0200

    Everything is working except validation for input

---
+++ TicTacToeConsole/Messages.cs
@@ -0,0 +1,9 @@
+﻿namespace TicTacToeConsole
+{
+    class Messages
+    {
+        public const string squareTaken = "Square already taken. Please choose another one!";
+        public const string winner = "{0} is the winner";
+        public const string wrongNumber = "Please choose a number between 1 and 9";
+    }
+}
--- TicTacToeConsole/Program.cs
+++ TicTacToeConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace TicTacToeConsole
 {
@@ -6,7 +7,90 @@ namespace TicTacToeConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] arr = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+            Board(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int lastChoice = 0;
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Choose where to place X (1-9)");
+
+                    int choice = Int32.Parse(Console.ReadLine());
+                    if (arr[choice] == 'x' || arr[choice] == 'o')
+                    {
+                        Console.WriteLine(Messages.squareTaken);
+                        i--;
+                        continue;
+                    }
+                    arr[choice] = 'x';
+                    lastChoice = choice;
+                }
+                else
+                {
+                    Console.WriteLine("Choose where to place O (1-9)");
+                    int choice = Int32.Parse(Console.ReadLine());
+                    if (arr[choice] == 'x' || arr[choice] == 'o')
+                    {
+                        Console.WriteLine(Messages.squareTaken);
+                        i--;
+                        continue;
+                    }
+                    arr[choice] = 'o';
+                    lastChoice = choice;
+                }                
+                Board(arr);
+                if (CheckForWinner(arr))
+                {
+                    Console.WriteLine(Messages.winner, arr[lastChoice]);
+                    break;
+                }
+            }
+
+        }
+
+        private static void Board(char[] arr)
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
+        }
+
+        private static bool CheckForWinner(char[] arr)
+        {
+            if (arr[1] == arr[2] && arr[2] == arr[3] && arr[3] != ' ') { return true; }
+            if (arr[4] == arr[5] && arr[5] == arr[6] && arr[6] != ' ') { return true; }
+            if (arr[7] == arr[8] && arr[8] == arr[9] && arr[9] != ' ') { return true; }
+
+            if (arr[1] == arr[4] && arr[4] == arr[7] && arr[7] != ' ') { return true; }
+            if (arr[2] == arr[5] && arr[5] == arr[8] && arr[8] != ' ') { return true; }
+            if (arr[3] == arr[6] && arr[6] == arr[9] && arr[9] != ' ') { return true; }
+
+            if (arr[1] == arr[5] && arr[5] == arr[9] && arr[9] != ' ') { return true; }
+            if (arr[3] == arr[5] && arr[5] == arr[7] && arr[7] != ' ') { return true; }
+
+            return false;
+
+        }
+
+        private static bool CheckIf1to9(string input)
+        {
+            Regex regex = new Regex("^[1-9]$");
+
+            // todo Int32.TryParse
+
+            if (regex.IsMatch(input))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
